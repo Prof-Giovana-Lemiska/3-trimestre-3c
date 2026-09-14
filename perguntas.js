920793978f4db98db452279a46a32b3c3c449b85
Giovana Lemiska <130910854+Prof-Giovana-Lemiska@users.noreply.github.com>
2026-09-14 11:49:14 -0300

    aa

--- perguntas.js
+++ perguntas.js
@@ -4,31 +4,31 @@ criarCartao(
     'Resposta'
 )
 
-criaCartao(
+criarCartao(
     'Programação',
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
 
-criaCartao(
+criarCartao(
     'Geografia',
     'Qual a capital da França?',
     'A capital da França é Paris'
 )
 
-criaCartao(
+criarCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
-criaCartao(
+criarCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
 
-criaCartao(
+criarCartao(
     'Lingua espanhola',
     'Como se diz oi em Espanhol?',
     'Oi em ingles é Holla (Ola)'
